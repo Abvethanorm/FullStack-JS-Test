90b1244bc342ca7fb9f8d38eb94098295b6626d6
Abvethanorm <norman.pegues@gmail.com>
2023-03-28 10:19:37 -0400

    dockerization added

--- root/server/server.js
+++ root/server/server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const app = require("./app");
 
 // App configuration
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 // Start server
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
